265bad835ca42fe6b23beba5e77140c010cb0083
omarhag23 <omarabbas300@gmail.com>
2024-03-27 00:25:58 +0200

    req outside controllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,19 +6,19 @@ const UserController = {
 //register
 registerUser :async (username,email,password) => {
     // Validation  to check if user exists!
-    const userExists = await User.findOne({email:req.body.email})
+    const userExists = await User.findOne({email:email})
     if(userExists){
       return res.status(400).send({message:'User already exists'})
     }
 
     // I created a hashed represenation of my password!
     const salt = await bcryptjs.genSalt(5)
-    const hashedPassword = await bcryptjs.hash(req.body.password,salt)
+    const hashedPassword = await bcryptjs.hash(password,salt)
 
     // Code to insert data
     const user = new User({
-        username:req.body.username,
-        email:req.body.email,
+        username:username,
+        email:email,
         password:hashedPassword
     })
     try{
