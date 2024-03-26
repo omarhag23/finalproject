88e5ae85eccea7e54c554a0614926352e90cadea
omarhag23 <omarabbas300@gmail.com>
2024-03-26 04:56:55 +0200

    index views

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const cartRoutes = require('./routes/cart');
 
 app.set('view engine', 'ejs');
 const path = require('path');
-app.set('views', path.join(__dirname, 'frontend'));
+app.set('views', path.join(__dirname, '..','frontend'));
 
 // Middleware
 app.use(cookieParser());
