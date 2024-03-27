88e53196a4d7f72071f62bee549f6f366fabb162
omarhag23 <omarabbas300@gmail.com>
2024-03-27 21:36:31 +0200

    yes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -38,7 +38,7 @@ router.post('/register', async (req, res) => {
         // Authentication successful
         console.error('authentication super OK',username, ' logged in,trying to redirect ')
         req.session.username = username;
-        res.status(200).json({ message: 'Authentication successful' });
+        res.status(200).json({ message: 'Authentication successfusl' });
         res.redirect('/');
       } else {
         // Authentication failed (invalid credentials)
