f79c461dad6f88b745d79f8688f1d8623595d3df
omarhag23 <omarabbas300@gmail.com>
2024-03-28 04:10:30 +0200

    order

--- backend/routes/shop.js
+++ backend/routes/shop.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
+router.use(express.static(path.join(__dirname, '..', 'frontend')));
 const router = express.Router();
 const Beat = require('../models/Beat');
 const Service = require('../models/Service');
 const Kit = require('../models/Kit');
-const path = require('path');
-router.use(express.static(path.join(__dirname, '..', 'frontend')));
+;
 
 router.get('/beats', async (req, res) => {
   try {
