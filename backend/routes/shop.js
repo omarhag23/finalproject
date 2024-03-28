5532f6afb628ca4040ca2e6668bd8e82af7afdc1
omarhag23 <omarabbas300@gmail.com>
2024-03-28 04:04:22 +0200

    try

--- backend/routes/shop.js
+++ backend/routes/shop.js
@@ -10,7 +10,7 @@ router.get('/beats', async (req, res) => {
   try {
     try {
       const beats = await Beat.find(); // Correct usage of Mongoose find() method
-      res.render('beats', { beats });
+      res.render('frontend/beats', { beats });
     } catch (error) {
       console.error('Big Error getting beats:', error);
       throw error;
