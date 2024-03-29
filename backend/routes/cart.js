3fd14bd0d32069f678e831fdd6a17997ef9c4890
omarhag23 <omarabbas300@gmail.com>
2024-03-29 04:23:53 +0200

    loo

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 const Cart = require('../models/Cart');
 let model;
 const path = require('path');
