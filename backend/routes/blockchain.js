c4962884ea7118930ca662ef8f4a082642fc6ac3
omarhag23 <omarabbas300@gmail.com>
2024-04-03 06:06:08 +0200

    debu

--- backend/routes/blockchain.js
+++ backend/routes/blockchain.js
@@ -2,7 +2,7 @@ const { Web3 } = require('web3');
 
 const fs = require('fs');
 
-const web3 = new Web3('HTTP://193.61.44.23:7545'); // Update with your Ganache RPC server address
+const web3 = new Web3('HTTP://193.61.44.35:7545'); // Update with your Ganache RPC server address
 
 
 
@@ -71,7 +71,10 @@ const buyerAddress =accounts[1];
                 arguments: [sellerAddress]
             });
             console.log("contract deployed, estimating gas... ");
-            const gas = await deploy.estimateGas();
+            const gas = await web3.eth.estimateGas({
+                data: bytecode
+            });;
+            console.log(" estimated gas :  ",gas); 
             const receipt = await deploy.send({
                 from: sellerAddress,
                 gas: gas,
