a4a1b49c4d76c5940bc0ac03f0935690d6ff4d2f
omarhag23 <omarabbas300@gmail.com>
2024-04-03 06:25:08 +0200

    c

--- backend/routes/blockchain.js
+++ backend/routes/blockchain.js
@@ -73,8 +73,7 @@ const buyerAddress =accounts[1];
             console.log("contract deployed, estimating gas... ");
             const as = '8750000';
        //     const gas = await deploy.estimateGas();
-            console.log("estimated gas:", gas);
-            console.log(" estimated gas :  ",gas); 
+            
             const receipt = await deploy.send({
                 from: sellerAddress,
                 gas: as,
