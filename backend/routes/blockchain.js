d09ffe7382e3b0d143f9f40d3c0a95ff7987d1aa
omarhag23 <omarabbas300@gmail.com>
2024-04-03 05:42:22 +0200

    s

--- backend/routes/blockchain.js
+++ backend/routes/blockchain.js
@@ -55,13 +55,14 @@ const bytecode = '608060405234801561000f575f80fd5b506040516105c53803806105c58339
 
 
 const accounts = web3.eth.accounts;
-console.log("accounts",accounts);
+
 const buyerAddress =accounts[1];
 const sellerAddress = accounts[0];
  
 
 const Blockchain = {
     deployContract: async () => {
+        console.log("accounts",accounts);
         try {
             const contract = new web3.eth.Contract(abi);
             console.log("contract found,trying to deploy, seller address : ",sellerAddress);
