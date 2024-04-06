8cbe32275dac928c8c12c4ac14b68be924357975
omarhag23 <omarabbas300@gmail.com>
2024-04-06 18:06:54 +0200

    save

--- backend/routes/blockchain.js
+++ backend/routes/blockchain.js
@@ -2,7 +2,7 @@ const { Web3 } = require('web3');
 
 const fs = require('fs');
 
-const web3 = new Web3('HTTP://193.61.44.35:7545'); // Update with your Ganache RPC server address
+const web3 = new Web3('HTTP://193.61.44.23:7545'); // Update with your Ganache RPC server address
 
 
 
